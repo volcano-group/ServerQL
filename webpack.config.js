a52b0c9750d0355284a49e8a26bf3b07654febef
servermeta <alberto.websci@gmail.com>
2017-04-26 16:26:31 +0200

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    filename: './dist/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
